cbfc1a01a4ab6a779f8ec469b4a747bc9f259a34
Avanti Shrikumar <avanti.shrikumar@gmail.com>
2016-09-06 18:52:12 -0700

    periodic commit

--- momma_dragonn/loaders.py
+++ momma_dragonn/loaders.py
@@ -1,12 +1,14 @@
 import yaml
 from avutils import file_processing as fp
 
+
 def load_class_from_config(config, extra_kwargs={}):
     config = load_file_if_string(config)
     the_class = config['class']
     kwargs = config['kwargs']
     kwargs.add_all(extra_kwargs)
     return the_class(**kwargs)
+
 
 def load_callbacks(config):
     config = load_file_if_string(config)
@@ -16,11 +18,13 @@ def load_callbacks(config):
                     for callback_config in config["end_of_training_callbacks"]] 
     return end_of_epoch_callbacks, end_of_training_callbacks 
 
+
 def load_file_if_string(config):
     if (isinstance(config, str)):
         config = yaml.load(fp.get_file_handle(config)) 
     return config
     
+
 def load_hyperparameter_configs(hyperparameter_configs_list):
     list_of_hyperparameter_settings = []   
     for hyperparameter_configs in\
--- momma_dragonn/model_creators/flexible_keras.py
+++ momma_dragonn/model_creators/flexible_keras.py
@@ -1,24 +1,43 @@
 from .core import AbstractModelCreator
 from avutils.dynamic_enum import Key, Keys
+import keras
+from momma_dragonn.loaders import loaders
 
-FlexibleKerasKeys = Keys(Key("layers"),
-                         Key("optimizer"),
-                         Key("objective"))
 
-class FlexibleKeras(AbstractModelCreator):
+class FlexibleKerasGraph(AbstractModelCreator):
     def __init__(self, config):
-        self.config = config
+        self.config = loaders.load_file_if_string(config)
 
     def get_model(self):
-        uncompiled_model = self._get_uncompiled_model() 
-        compiled_model = self._compile_model(uncompiled_model)
-        return compiled_model
+        model = self._get_uncompiled_model() 
+        self._compile_model(model)
+        return model
 
     def _get_uncompiled_model(self):
-        raise NotImplementedError()
+        from keras.model import Graph 
+        graph = Graph()
+        self._add_input_layers(graph) 
+        self._add_nodes(graph)
+        return graph
 
-    def _compile_model(self, model):
-        raise NotImplementedError()
+    def _add_input_layers(self, graph):
+        input_layers_config = self.config["input_layers"]
+        for input_layer_config in input_layers_config:
+           graph.add_input(name=input_layers_config["name"],
+                           input_shape=input_layers_config["input_shape"]) 
+
+    def _add_nodes(self, graph):
+        nodes_config = self.config["nodes"]
+        for node_config in nodes_config:
+            class_and_kwargs = node_config["class_and_kwargs"]
+            layer = load_class_from_config(class_and_kwargs, extra_kwargs={}):
+            graph.add_node(layer, name=node_config["name"],
+                           input=node_config["input_name"])
+
+    def _compile_model(self, graph):
+        optimizer = load_class_from_config(self.config["optimizer"])
+        loss_dictionary = self.config["loss_dictionary"]
+        graph.compile(optimizer=optimizer, loss=loss_dictionary);         
 
     def get_config(self):
         return self.config
