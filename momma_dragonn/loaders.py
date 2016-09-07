4216033727c0a78ba3fbdb7de8f12cd7dd4c99aa
Avanti Shrikumar <avanti.shrikumar@gmail.com>
2016-09-06 17:14:23 -0700

    periodic commit

--- momma_dragonn/loaders.py
+++ momma_dragonn/loaders.py
@@ -21,3 +21,20 @@ def load_file_if_string(config):
         config = yaml.load(fp.get_file_handle(config)) 
     return config
     
+def load_hyperparameter_configs(hyperparameter_configs_list):
+    list_of_hyperparameter_settings = []   
+    for hyperparameter_configs in\
+        load_file_if_string(hyperparameter_configs_list):
+        other_data_loaders = [load_class_from_config(data_loader_config)
+                              for data_loader_config in
+                              hyperparameter_configs["other_data_loaders"]]
+        model_creator = load_class_from_config(
+                            hyperparameter_configs["model_creator"]) 
+        model_trainer = load_class_from_config(
+                            hyperparameter_configs["model_trainer"])
+        list_of_hyperparameter_settings.append(
+            {'other_data_loaders':other_data_loaders,
+             'model_creator':model_creator,
+             'model_trainer':model_trainer})
+    return list_of_hyperparameter_settings
+         
