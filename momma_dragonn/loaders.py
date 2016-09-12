69c6bd94089e57f62d617ff6170b50b4db4eeb82
Avanti Shrikumar <avanti.shrikumar@gmail.com>
2016-09-11 22:10:40 -0700

    debugging in progress

--- momma_dragonn/loaders.py
+++ momma_dragonn/loaders.py
@@ -5,30 +5,30 @@ from avutils import util
 
 
 def load_class_from_config(config, extra_kwargs={}, module_prefix=""):
-    config = util.load_yaml_if_string(config)
+    config = fp.load_yaml_if_string(config)
     the_class = module_prefix+config['class']
     kwargs = config['kwargs']
-    kwargs.add_all(extra_kwargs)
+    kwargs.update(extra_kwargs)
     return the_class(**kwargs)
 
 
-def load_data_loader(config, extra_kwargs):
+def load_data_loader(config, extra_kwargs={}):
     return load_class_from_config(config=config, extra_kwargs=extra_kwargs,
                 module_prefix="momma_dragonn.data_loaders.")
 
 
-def load_stopping_criterion(config, extra_kwargs):
+def load_stopping_criterion(config, extra_kwargs={}):
     return load_class_from_config(config=config, extra_kwargs=extra_kwargs,
                 module_prefix="momma_dragonn.stopping_criteria.")
 
 
-def load_model_evaluator(config, extra_kwargs):
+def load_model_evaluator(config, extra_kwargs={}):
     return load_class_from_config(config=config, extra_kwargs=extra_kwargs,
                 module_prefix="momma_dragonn.model_evaluators.")
 
 
 def load_end_of_epoch_callbacks(config):
-    config = util.load_yaml_if_string(config)
+    config = fp.load_yaml_if_string(config)
     end_of_epoch_callbacks = [
         load_class_from_config(config=callback_config,
             module_prefix="momma_dragonn.end_of_epoch_callbacks.")
@@ -37,7 +37,7 @@ def load_end_of_epoch_callbacks(config):
 
 
 def load_end_of_training_callbacks(config, key_metric_name):
-    config = util.load_yaml_if_string(config)
+    config = fp.load_yaml_if_string(config)
     end_of_training_callbacks = [
         load_class_from_config(config=callback_config,
             extra_kwargs={'key_metric_name': key_metric_name},
@@ -49,7 +49,7 @@ def load_end_of_training_callbacks(config, key_metric_name):
 def load_hyperparameter_configs(hyperparameter_configs_list):
     list_of_hyperparameter_settings = []   
     for hyperparameter_configs in\
-        util.load_yaml_if_string(hyperparameter_configs_list):
+        fp.load_yaml_if_string(hyperparameter_configs_list):
         other_data_loaders = OrderedDict([
             (split_name, load_class_from_config(data_loader_config))
             for (split_name, data_loader_config) in
@@ -64,5 +64,4 @@ def load_hyperparameter_configs(hyperparameter_configs_list):
             {'other_data_loaders':other_data_loaders,
              'model_creator':model_creator,
              'model_trainer':model_trainer})
-    return list_of_hyperparameter_settings
-         
+    return list_of_hyperparameter_settings 
