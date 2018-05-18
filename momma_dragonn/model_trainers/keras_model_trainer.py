b5e208ec84a78e8c7f5ed4b41dcda1d97645ccf3
Avanti Shrikumar <avanti.shrikumar@gmail.com>
2018-05-18 15:40:13 -0700

    dev for reparameterization

--- momma_dragonn/model_trainers/keras_model_trainer.py
+++ momma_dragonn/model_trainers/keras_model_trainer.py
@@ -12,12 +12,14 @@ class KerasFitGeneratorModelTrainer(AbstractModelTrainer):
                        class_weight=None,
                        seed=1234,
                        csv_logger=None,
-                       report_all_valid_metrics_every_epoch=False):
+                       report_all_valid_metrics_every_epoch=False,
+                       reparameterizer=None):
         self.seed = seed 
         self.samples_per_epoch = samples_per_epoch 
         self.stopping_criterion_config = stopping_criterion_config
         self.report_all_valid_metrics_every_epoch =\
             report_all_valid_metrics_every_epoch
+        self.reparameterizer = reparameterizer
         if (class_weight is not None):
             self.class_weight = dict((int(key),val) for
                                       key,val in class_weight.items())
@@ -33,7 +35,10 @@ class KerasFitGeneratorModelTrainer(AbstractModelTrainer):
                 ('stopping_criterion_config', self.stopping_criterion_config),
                 ('class_weight', self.class_weight),
                 ('report_all_valid_metrics_every_epoch',
-                  self.report_all_valid_metrics_every_epoch)])
+                  self.report_all_valid_metrics_every_epoch),
+                ('reparameterizer', 
+                  None if self.reparameterizer is None
+                       else self.reparameterizer.get_jsonable_object())])
  
     def train(self, model_creator, model_evaluator,
                     valid_data_loader, other_data_loaders,
@@ -68,23 +73,43 @@ class KerasFitGeneratorModelTrainer(AbstractModelTrainer):
         performance_history = PerformanceHistory()
 
         training_metadata = OrderedDict() 
-        epoch_external = util.VariableWrapper(0)
         best_valid_perf_finder = util.init_get_best(is_larger_better)
 
-        class MommaDragonnEpochEndCallback(keras.callbacks.Callback):
+        #additional user-supplied callbacks
+        extra_callbacks=[]
+        if self.csv_logger!=None:
+            from keras.callbacks import CSVLogger
+            csv_logger=CSVLogger(self.csv_logger,append=True)
+            extra_callbacks.append(csv_logger) 
+        try:
 
-            def on_epoch_begin(self, epoch, logs=None):
+            epochs_trained = 0
+
+            while stopping_criterion.stop_training()==False:
+
                 for start_of_epoch_callback in start_of_epoch_callbacks:
                     start_of_epoch_callback( #written for snapshot saving
-                        epoch=epoch,
+                        epoch=epochs_trained,
                         model_wrapper=model_wrapper)
 
-            def on_epoch_end(self, epoch, logs=None):
-                
-                epoch_external.var = epoch
- 
-                train_data_for_eval = train_data_loader.get_data_for_eval()
+                model_wrapper.get_model().fit_generator(
+                    train_data_loader.get_batch_generator(),
+                    samples_per_epoch=self.samples_per_epoch,
+                    nb_epoch=1,
+                    class_weight=self.class_weight,
+                    callbacks=[]+extra_callbacks)
 
+                #increment the epoch and do stuff at the end of the epoch
+                epochs_trained += 1
+
+                if (self.reparameterizer):
+                    if (self.reparameterizer.do_reparameterization(
+                                              epoch=epochs_trained)):
+                        print("REPARAMETERIZING!!!")
+                        model_wrapper.set_model(
+                            self.reparameterizer(model_wrapper.get_model()))
+
+                train_data_for_eval = train_data_loader.get_data_for_eval()
                 train_key_metric = model_evaluator.compute_key_metric(
                                     model_wrapper=model_wrapper,
                                     data=train_data_for_eval,
@@ -93,12 +118,9 @@ class KerasFitGeneratorModelTrainer(AbstractModelTrainer):
                                     model_wrapper=model_wrapper,
                                     data=valid_data,
                                     batch_size=train_data_loader.batch_size)
-
                 new_best = best_valid_perf_finder.process(epoch,
                                                           valid_key_metric)
-
                 stopping_criterion.update(valid_key_metric)
-
                 if (report_all_valid_metrics_every_epoch):
                     this_epoch_valid_all_stats =\
                                 model_evaluator.compute_all_stats(
@@ -107,13 +129,10 @@ class KerasFitGeneratorModelTrainer(AbstractModelTrainer):
                                     batch_size=train_data_loader.batch_size)
                 else:
                     this_epoch_valid_all_stats = {}
-
                 performance_history.epoch_update(
                     train_key_metric=train_key_metric,
                     valid_key_metric=valid_key_metric,
                     valid_all_stats = this_epoch_valid_all_stats)
-                    
-
                 if (new_best):
                     print("New best")
                     if (not report_all_valid_metrics_every_epoch):
@@ -128,50 +147,33 @@ class KerasFitGeneratorModelTrainer(AbstractModelTrainer):
                         epoch=epoch, valid_key_metric=valid_key_metric,
                         train_key_metric=train_key_metric,
                         valid_all_stats=self.valid_all_stats)
-
                 for end_of_epoch_callback in end_of_epoch_callbacks:
                     end_of_epoch_callback(#handles intermediate model saving
-                        epoch=epoch,
+                        epoch=epochs_trained,
                         model_wrapper=model_wrapper,
                         valid_key_metric=valid_key_metric,
                         train_key_metric=train_key_metric,
                         valid_all_stats=self.valid_all_stats,
                         is_new_best_valid_perf=new_best,
                         performance_history=performance_history)
-
-                if (stopping_criterion.stop_training()):
-                    self.model.stop_training = True 
-
-        #additional user-supplied callbacks
-        extra_callbacks=[]
-        if self.csv_logger!=None:
-            from keras.callbacks import CSVLogger
-            csv_logger=CSVLogger(self.csv_logger,append=True)
-            extra_callbacks.append(csv_logger) 
-        try:
-            model_wrapper.get_model().fit_generator(
-                train_data_loader.get_batch_generator(),
-                samples_per_epoch=self.samples_per_epoch,
-                nb_epoch=10000,
-                class_weight=self.class_weight,
-                callbacks=[MommaDragonnEpochEndCallback()]+extra_callbacks)
+                
             training_metadata['termination_condition'] = "normal"
         except (KeyboardInterrupt):
-            print("\nTraining was interrupted at epoch ",
-                     epoch_external.var,"with a keyboard interrupt")
+            print("\nTraining was interrupted after epoch ",
+                     epochs_trained,"with a keyboard interrupt")
             training_metadata['termination_condition']= "KeyboardInterrupt"
         except Exception as e:
             traceback_str = str(traceback.format_exc())
             print(traceback_str) 
-            print("\nTraining was interrupted at epoch ",
-                     epoch_external.var,"with an exception")
+            print("\nTraining was interrupted after epoch ",
+                     epochs_trained,"with an exception")
             for error_callback in error_callbacks:
                 error_callback(exception=e,
                                traceback_str=traceback_str,
-                               epoch=epoch_external.var)
+                               epoch=epochs_trained)
             training_metadata['termination_condition']= str(type(e)) 
         finally:
-            training_metadata['total_epochs_trained_for']=epoch_external.var
+            training_metadata['total_epochs_trained_for']=epochs_trained
 
         return model_wrapper, performance_history, training_metadata
         
