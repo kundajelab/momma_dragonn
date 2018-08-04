ad17051289dae90f489f6b67e3fa38b2ed0edd96
Jocelin Su <jocelin.su@gmail.com>
2018-08-03 21:22:16 -0700

    Added pyfasta data loader + utilities

---
+++ examples/pyfasta_sequential_model/run_model.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+momma_dragonn_train #will rely on default names for the config files
--- momma_dragonn/data_loaders/__init__.py
+++ momma_dragonn/data_loaders/__init__.py
@@ -3,3 +3,4 @@ from . import core
 from . import hdf5_data_loader
 from . import bed_fasta_data_loader
 from . import genomelake_data_loader
+from . import pyfasta_data_loader
