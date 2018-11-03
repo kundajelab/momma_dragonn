cd1b5b4d56592c4e3c60cd243b42e7e383da601c
AvantiShri <avanti.shrikumar@gmail.com>
2018-11-02 21:25:35 -0700

    twostream data loader works

--- momma_dragonn/data_loaders/pyfasta_data_loader.py
+++ momma_dragonn/data_loaders/pyfasta_data_loader.py
@@ -109,6 +109,52 @@ class AbstractSeqOnlyDataLoader(AbstractBatchDataLoader):
             return util.enum(X=np.array(X), Y=np.array(Y))
 
 
+def get_pyfasta_generator(bed_source, fasta_data_source,
+                          append_chrom_number, labels_dtype,
+                          randomize_after_pass,
+                          random_seed, loop_infinitely):
+    #read bed_source into memory
+    bed_fh = fp.get_file_handle(bed_source)
+    data = []
+    print("Reading bed file "+bed_source+" into memory")
+    for a_row in bed_fh:
+        a_row = a_row.rstrip().split("\t")
+        data.append(Interval(
+            chrom=a_row[0], start=int(a_row[1]), stop=int(a_row[2]),
+            labels=[labels_dtype(x) for x in a_row[3:]]))
+    print("Finished reading bed file into memory; got "
+          +str(len(data))+"rows")
+    random_obj = np.random.RandomState(random_seed)
+    if (randomize_after_pass):
+        data = shuffle_array(arr=data, random_obj=random_obj)
+
+    #fasta extraction
+    import pyfasta
+    f = pyfasta.Fasta(fasta_data_source)
+
+    idx = 0
+    while (idx < len(data)):
+        to_extract = data[idx:idx+1]
+        chrom = to_extract[0].chrom
+        if (append_chrom_number == True):
+            chrom = chrom+" "+chrom[3:]
+        to_yield = f[chrom][to_extract[0].start:to_extract[0].stop]
+        to_yield = np.array([one_hot_encode[x] for x in to_yield])
+        yield (to_yield, to_extract[0].labels,
+               (to_extract[0].chrom,
+                to_extract[0].start,
+                to_extract[0].stop))
+
+        idx += 1
+        if (idx==len(data)):
+            if (loop_infinitely):
+                if (randomize_after_pass):
+                    data = shuffle_array(arr=data, random_obj=random_obj)
+                idx=0
+            else:
+                raise StopIteration()
+
+
 class SingleStreamSeqOnly(AbstractSeqOnlyDataLoader):
 
     def __init__(self, batch_size,
@@ -144,50 +190,77 @@ class SingleStreamSeqOnly(AbstractSeqOnlyDataLoader):
         return the_dict
 
     def get_generator(self, loop_infinitely):
-        #read bed_source into memory
-        bed_fh = fp.get_file_handle(self.bed_source)
-        data = []
-        print("Reading bed file "+self.bed_source+" into memory")
-        for a_row in bed_fh:
-            a_row = a_row.rstrip().split("\t")
-            data.append(Interval(
-                chrom=a_row[0], start=int(a_row[1]), stop=int(a_row[2]),
-                labels=[self.labels_dtype(x) for x in a_row[3:]]))
-        print("Finished reading bed file into memory; got "
-              +str(len(data))+"rows")
-        if (self.num_to_load_for_eval > len(data)):
-            print("num_to_load_for_eval is "+str(self.num_to_load_for_eval)
-                  +" but length of data is "+str(len(data))+"; adjusting")
-            self.num_to_load_for_eval = len(data)
-        random_obj = np.random.RandomState(self.random_seed)
-        if (self.randomize_after_pass):
-            data = shuffle_array(arr=data, random_obj=random_obj)
+        return get_pyfasta_generator(bed_source=self.bed_source,
+                              fasta_data_source=self.fasta_data_source,
+                              append_chrom_number=self.append_chrom_number,
+                              labels_dtype=self.labels_dtype,
+                              randomize_after_pass=self.randomize_after_pass,
+                              random_seed=self.random_seed,
+                              loop_infinitely=loop_infinitely)
 
-        #fasta extraction
-        import pyfasta
-        f = pyfasta.Fasta(self.fasta_data_source)
 
-        idx = 0
-        while (idx < len(data)):
-            to_extract = data[idx:idx+1]
-            chrom = to_extract[0].chrom
-            if (self.append_chrom_number == True):
-                chrom = chrom+" "+chrom[3:]
-            to_yield = f[chrom][to_extract[0].start:to_extract[0].stop]
-            to_yield = np.array([one_hot_encode[x] for x in to_yield])
-            yield (to_yield, to_extract[0].labels,
-                   (to_extract[0].chrom,
-                    to_extract[0].start,
-                    to_extract[0].stop))
+class TwoStreamSeqOnly(AbstractSeqOnlyDataLoader):
 
-            idx += 1
-            if (idx==len(data)):
-                if (loop_infinitely):
-                    if (self.randomize_after_pass):
-                        data = shuffle_array(arr=data, random_obj=random_obj)
-                    idx=0
-                else:
-                    raise StopIteration()
+    def __init__(self, batch_size,
+                       positives_bed_source,
+                       negatives_bed_source,
+                       negatives_to_positives_ratio,
+                       fasta_data_source,
+                       rc_augment,
+                       num_to_load_for_eval,
+                       randomize_after_pass=True,
+                       random_seed=1,
+                       labels_dtype="int",
+                       wrap_in_keys=None,
+                       append_chrom_number=False):
+        super(TwoStreamSeqOnly, self).__init__(
+            batch_size=batch_size,
+            rc_augment=rc_augment,
+            num_to_load_for_eval=num_to_load_for_eval,
+            wrap_in_keys=wrap_in_keys)
+        self.positives_bed_source = positives_bed_source
+        self.negatives_bed_source = negatives_bed_source
+        self.negatives_to_positives_ratio = negatives_to_positives_ratio
+        assert isinstance(negatives_to_positives_ratio, int)
+        self.fasta_data_source = fasta_data_source
+        self.str_labels_dtype = labels_dtype
+        self.randomize_after_pass = randomize_after_pass
+        self.random_seed = random_seed
+        self.labels_dtype=eval(labels_dtype)
+        self.append_chrom_number = append_chrom_number
+
+    def get_jsonable_object(self):
+        the_dict = super(TwoStreamSeqOnly, self).get_jsonable_object()
+        the_dict['bed_source'] = self.bed_source
+        the_dict['fasta_data_source'] = self.fasta_data_source
+        the_dict['labels_dtype'] = self.str_labels_dtype
+        the_dict['randomize_after_pass'] = self.randomize_after_pass
+        the_dict['random_seed'] = self.random_seed
+        return the_dict
+
+    def get_generator(self, loop_infinitely):
+        assert loop_infinitely==True,\
+            "TwoStream not supported for no infinite looping"
+        positives_generator = get_pyfasta_generator(
+                    bed_source=self.positives_bed_source,
+                    fasta_data_source=self.fasta_data_source,
+                    append_chrom_number=self.append_chrom_number,
+                    labels_dtype=self.labels_dtype,
+                    randomize_after_pass=self.randomize_after_pass,
+                    random_seed=self.random_seed,
+                    loop_infinitely=loop_infinitely)
+        negatives_generator = get_pyfasta_generator(
+                    bed_source=self.negatives_bed_source,
+                    fasta_data_source=self.fasta_data_source,
+                    append_chrom_number=self.append_chrom_number,
+                    labels_dtype=self.labels_dtype,
+                    randomize_after_pass=self.randomize_after_pass,
+                    random_seed=self.random_seed,
+                    loop_infinitely=loop_infinitely)
+        while 1:
+            yield positives_generator.next() 
+            for i in range(self.negatives_to_positives_ratio):
+                yield negatives_generator.next()
 
 
 #randomly shuffles the input array
